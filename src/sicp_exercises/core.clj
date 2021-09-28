3fbcfd925b48658eb607176844469659e9c720b0
dicky.kamarulah <dicky.kamarulah@go-jek.com>
2021-09-28 10:43:10 +0700

    adding procedures that takes three numbers as arguments and returns the sum of the squares

--- src/sicp_exercises/core.clj
+++ src/sicp_exercises/core.clj
@@ -1,6 +1,11 @@
 (ns sicp-exercises.core)
 
-(defn foo
-  "I don't do a whole lot."
-  [x]
-  (println x "Hello, World!"))
+;; Exercises Chapter 1
+(defn largest-squares [a b c]
+  (cond
+    (and (> a b) (> b c)) (+ (* a a) (* b b))
+    (and (> b a) (> a c)) (+ (* a a) (* b b))
+    (and (> a b) (> c b)) (+ (* a a) (* c c))
+    (and (> c a) (> a b)) (+ (* a a) (* c c))
+    (and (> b a) (> c a)) (+ (* b b) (* c c))
+    (and (> c a) (> b a)) (+ (* b b) (* c c))))
--- test/sicp_exercises/core_test.clj
+++ test/sicp_exercises/core_test.clj
@@ -2,6 +2,19 @@
   (:require [clojure.test :refer :all]
             [sicp-exercises.core :refer :all]))
 
-(deftest a-test
-  (testing "FIXME, I fail."
-    (is (= 0 1))))
+(deftest largest-squares-core-test
+  (testing "a and b is the biggest"
+    (testing "a is bigger then b"
+      (is (= 13 (largest-squares 3 2 1))))
+    (testing "b is bigger then a"
+      (is (= 13 (largest-squares 2 3 1)))))
+  (testing "b and c is the biggest"
+    (testing "b is bigger then c"
+      (is (= 13 (largest-squares 1 3 2))))
+    (testing "c is bigger then b"
+      (is (= 13 (largest-squares 1 2 3)))))
+  (testing "a and c is the biggest"
+    (testing "a is bigger then c"
+      (is (= 13 (largest-squares 3 1 2))))
+    (testing "c is bigger then a"
+      (is (= 13 (largest-squares 2 1 3))))))
